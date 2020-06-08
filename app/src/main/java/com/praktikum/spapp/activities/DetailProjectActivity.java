aa094b395ed62afb79b9f0218ce7694125300619
Truong <Truong@students.uni-marburg.de>
2020-06-08 13:56:42 +0200

    project detail and view skeleton implementation

--- app/src/main/java/com/praktikum/spapp/activities/DetailProjectActivity.java
+++ app/src/main/java/com/praktikum/spapp/activities/DetailProjectActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.praktikum.spapp.R;
+import com.praktikum.spapp.models.Project;
 import com.praktikum.spapp.models.User;
 
 import org.json.JSONObject;
@@ -17,20 +18,20 @@ import okhttp3.RequestBody;
 import static com.praktikum.spapp.Service.Service.JSON;
 
 public class DetailProjectActivity extends AppCompatActivity{
-    //string from intent
-    String userName, userPassword, studentNumber, firstName, lastName, invitationLink;
-    String projectNr, projectName;
-    //string from xml elems
-    String givenName, givenPWD, givenSN, givenMJ, givenPO;
-    //XML elems
-    EditText uname, pwd, matnr, major, po;
-    Button buttonConfirm, buttonCancel;
+    private TextView projectNr, projectName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_project);
 
+        Project project = (Project) getIntent().getSerializableExtra("user");
+
+        projectNr = (TextView) findViewById(R.id.projectNr);
+        projectNr.setText(project.getId());
+
+        projectName = (TextView) findViewById(R.id.projectName);
+        projectNr.setText(project.getId());
     }
 
 }
