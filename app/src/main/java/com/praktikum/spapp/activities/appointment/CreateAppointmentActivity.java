f1212a24819a750a714d2d60e36612e97219490a
requireSol <enamsolaimani@icloud.com>
2020-06-27 16:42:36 +0200

    add activity for appointments

---
+++ app/src/main/java/com/praktikum/spapp/activities/appointment/CreateAppointmentActivity.java
@@ -0,0 +1,16 @@
+package com.praktikum.spapp.activities.appointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.praktikum.spapp.R;
+
+public class CreateAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_appointment);
+    }
+}
---
+++ app/src/main/java/com/praktikum/spapp/activities/appointment/UpdateAppointmentActicity.java
@@ -0,0 +1,16 @@
+package com.praktikum.spapp.activities.appointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.praktikum.spapp.R;
+
+public class UpdateAppointmentActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_appointment_acticity);
+    }
+}
