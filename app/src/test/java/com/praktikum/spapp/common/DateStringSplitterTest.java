158556931aee0477c212fd0658bce2f96b235a7c
requireSol <enamsolaimani@icloud.com>
2020-06-28 22:21:25 +0200

    finish datepicker and timepicker with spinner type and create date iso format

--- app/src/test/java/com/praktikum/spapp/common/DateStringSplitterTest.java
+++ app/src/test/java/com/praktikum/spapp/common/DateStringSplitterTest.java
@@ -9,4 +9,9 @@ public class DateStringSplitterTest {
     public void datePrettyPrintTest() {
         System.out.println(DateStringSplitter.datePrettyPrint("2020-07-03T14:54:01.654734+02:00"));
     }
+
+    @Test
+    public void changeToDateFormat(){
+        System.out.print(DateStringSplitter.changeToDateFormat("01.5.2004","20:55 Uhr"));
+    }
 }
