22365cdf1029b8881d925b4b942c624ccb9b52eb
Truong <Truong@students.uni-marburg.de>
2020-07-17 18:57:45 +0200

    cleanup UI, ML tested

--- app/src/main/java/com/praktikum/spapp/activities/CommentDetailsActivity.java
+++ app/src/main/java/com/praktikum/spapp/activities/CommentDetailsActivity.java
@@ -1,45 +1,43 @@
 package com.praktikum.spapp.activities;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.snackbar.Snackbar;
 import com.praktikum.spapp.R;
+import com.praktikum.spapp.common.DateStringSplitter;
 import com.praktikum.spapp.models.Comment;
-import com.praktikum.spapp.models.Project;
 import com.praktikum.spapp.service.CommentService;
-import com.praktikum.spapp.service.UserService;
 import com.praktikum.spapp.common.Utils;
-import com.praktikum.spapp.models.EditUserForm;
-import com.praktikum.spapp.models.User;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static java.lang.Integer.parseInt;
 
 public class CommentDetailsActivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText email;
-    private EditText vorname;
-    private EditText nachname;
-    private EditText matrikelnummer;
-    private EditText studiengang;
-    private EditText pruefungsordnung;
+    private TextView username;
+    private TextView email;
+    private TextView vorname;
+    private TextView nachname;
+    private TextView matrikelnummer;
+    private TextView studiengang;
+    private TextView pruefungsordnung;
 
     private EditText commentContent;
-    private EditText commentDate;
+    private TextView commentDate;
     private Switch commentIsRestricted;
     private Switch commentIsEdited;
+
+    private Button button_edit_comment_and_cancel;
+    private Button button_edit_comment_save;
 
     private Button commentDeleteButton;
     private Button commenSetRestrictedButton;
@@ -55,13 +53,6 @@ public class CommentDetailsActivity extends AppCompatActivity {
 
         Comment comment = (Comment) getIntent().getSerializableExtra("comment");
 
-
-        /** not using edit for now
-        Button buttonEaC = findViewById(R.id.button_edit_comment_and_cancel);
-        Button buttonEditSave = findViewById(R.id.button_edit_comment_save);
-         */
-
-        //set bind ETs and set values
         username = findViewById(R.id.et_username);
         username.setText(comment.getAuthor().getUsername());
 
@@ -88,98 +79,85 @@ public class CommentDetailsActivity extends AppCompatActivity {
 
 
         commentDate = findViewById(R.id.et_comment_date);
-        commentDate.setText(comment.getCreationTime());
+        commentDate.setText(DateStringSplitter.datePrettyPrint(comment.getCreationTime()) + " " + DateStringSplitter.timePrettyPrint(comment.getCreationTime()));
 
         commentIsRestricted = findViewById(R.id.switch_comment_restricted);
         commentIsRestricted.setChecked(comment.isRestricted());
+        commentIsRestricted.setClickable(false);
 
         commentIsEdited = findViewById(R.id.switch_comment_edited);
-        commentIsEdited.setChecked(comment.isEdited());
+        commentIsEdited.setChecked(comment.isWasEdited());
+        commentIsEdited.setClickable(false);
 
         commentDeleteButton = findViewById(R.id.comment_delete_button);
         commenSetRestrictedButton = findViewById(R.id.comment_set_restricted);
         commentSetPublicButton = findViewById(R.id.comment_set_public);
 
-        /**
-        buttonEaC.setOnClickListener((View view) -> {
+        button_edit_comment_and_cancel = findViewById(R.id.button_edit_comment_and_cancel);
+        button_edit_comment_save = findViewById(R.id.button_edit_comment_save);
+        button_edit_comment_and_cancel.setOnClickListener((View view) -> {
 
             if (!editMode.get()) {
                 editMode.set(true);
-                //username.setEnabled(true);
+                commentContent.setEnabled(true);
+                button_edit_comment_and_cancel.setText("Cancel");
+                button_edit_comment_save.setVisibility(View.VISIBLE);
 
-                buttonEaC.setText("Cancel");
-                buttonEditSave.setVisibility(View.VISIBLE);
 
-
-                buttonEditSave.setOnClickListener(new View.OnClickListener() {
+                button_edit_comment_save.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        EditUserForm editUserForm = new EditUserForm();
-                        //editUserForm.setUsername(username.getText().toString());
-
+                        comment.setContent(commentContent.getText().toString());
+                        commentIsEdited.setChecked(comment.isWasEdited());
                         new Thread(() -> {
                             try {
-                                String responseString = new UserService().editUser(editUserForm);
+                                String responseString = new CommentService().commentEdit(comment.getId(),comment);
                                 if (Utils.isSuccess(responseString)) {
                                     runOnUiThread(() -> {
-
                                         editMode.set(false);
-                                        //username.setEnabled(false);
-                                        buttonEaC.setText("Edit - dont click");
-                                        buttonEditSave.setVisibility(View.GONE);
+                                        commentContent.setEnabled(false);
 
-                                        //username.setText(editUserForm.getUsername());
-                                        Snackbar.make(view, "Con fuckign gratys, your changes were saved.", Snackbar.LENGTH_LONG).show();
+                                        button_edit_comment_and_cancel.setText("Edit");
+                                        button_edit_comment_save.setVisibility(View.GONE);
 
-
+                                        Snackbar.make(view, "Your changes were saved.", Snackbar.LENGTH_LONG).show();
+                                        commentIsEdited.setChecked(true);
                                     });
                                 } else {
                                     runOnUiThread(() -> {
                                         Snackbar.make(view, Utils.parseForJsonObject(responseString, "Error"), Snackbar.LENGTH_LONG).show();
                                     });
-
                                 }
                             } catch (Exception e) {
                                 runOnUiThread(() -> {
                                     Snackbar.make(view, "Whoops, something went wrong.", Snackbar.LENGTH_LONG).show();
                                 });
                             }
-
-
                         }).start();
                     }
-
                 });
-
-
             } else {
-                //username.setText(comment.getAuthor().getUsername());
+                commentContent.setText(comment.getContent());
 
                 editMode.set(false);
-                //username.setEnabled(false);
-                buttonEaC.setText("Edit - dont click");
-                buttonEditSave.setVisibility(View.GONE);
+                commentContent.setEnabled(false);
 
-
+                button_edit_comment_and_cancel.setText("Edit");
+                button_edit_comment_save.setVisibility(View.GONE);
             }
-
-
-
         });
-        */
 
 
         commentDeleteButton.setOnClickListener((View view) -> {
             new Thread(() -> {
                 try {
-                    //HARDCODE FOR TESTING
                     String responseString = new CommentService().commentDelete(comment.getId());
-
                     if (Utils.isSuccess(responseString)) {
                         Snackbar.make(view, "Your changes were saved.", Snackbar.LENGTH_LONG).show();
                     } else {
                         Snackbar.make(view, Utils.parseForJsonObject(responseString, "Error"), Snackbar.LENGTH_LONG).show();
-                    }                } catch (Exception e) {
+                    }
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }).start();
@@ -193,6 +171,14 @@ public class CommentDetailsActivity extends AppCompatActivity {
                     String responseString = new CommentService().commentEdit(comment.getId(),comment);
                     if (Utils.isSuccess(responseString)) {
                         Snackbar.make(view1, "Your changes were saved.", Snackbar.LENGTH_LONG).show();
+                        Looper.prepare();
+                        Handler handler = new Handler();
+                        handler.post(new Runnable() {
+                            public void run() {
+                                commentIsRestricted.setChecked(comment.isRestricted());
+                            }
+                        });
+                        Looper.loop();
                     } else {
                         Snackbar.make(view1, Utils.parseForJsonObject(responseString, "An error has occurred"), Snackbar.LENGTH_LONG).show();
                     }
@@ -210,6 +196,14 @@ public class CommentDetailsActivity extends AppCompatActivity {
                     String responseString = new CommentService().commentEdit(comment.getId(),comment);
                     if (Utils.isSuccess(responseString)) {
                         Snackbar.make(view1, "Your changes were saved.", Snackbar.LENGTH_LONG).show();
+                        Looper.prepare();
+                        Handler handler = new Handler();
+                        handler.post(new Runnable() {
+                            public void run() {
+                                commentIsRestricted.setChecked(comment.isRestricted());
+                            }
+                        });
+                        Looper.loop();
                     } else {
                         Snackbar.make(view1, Utils.parseForJsonObject(responseString, "An error has occurred"), Snackbar.LENGTH_LONG).show();
                     }
